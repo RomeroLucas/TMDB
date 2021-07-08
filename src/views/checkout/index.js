1f46be68c28da7e94fc13aea6c63b24403eeaf37
Lucas Romero <lucas.romerovivas@gmail.com>
2021-07-08 15:26:57 -0300

    checkout form

--- src/views/checkout/index.js
+++ src/views/checkout/index.js
@@ -1,9 +1,35 @@
-import { Link } from 'react-router-dom'
+
+import './style.css'
 
 export default function Checkout() {
     return (
-        <main className='main-home'>
-            Olá Checkout <Link to='/'>go to Home</Link>
+        <main className='main-checkout'>
+            <h1>Finalizar Compra</h1>
+
+            <form>
+                {/* formulario esquerda */}
+                <section>
+                    <div><input className='input-full' type='text' placeholder='Nome Completo' /></div>
+                    <div>
+                        <input className='input-half' style={{width: '47%'}} type='text' placeholder='CPF' />
+                        <input className='input-half' style={{width: '47%'}} type='text' placeholder='Celular' />
+                    </div>
+                    <div><input className='input-full' type='text' placeholder='E-mail' /></div>
+                    <div>
+                        <input className='input-half' style={{width: '35%'}} type='text' placeholder='CEP' />
+                        <input className='input-half' style={{width: '60%'}} type='text' placeholder='Endereço' />
+                    </div>
+                    <div>
+                        <input className='input-half' style={{width: '47%'}} type='text' placeholder='Cidade' />
+                        <input className='input-half' style={{width: '47%'}} type='text' placeholder='Estado' />
+                    </div>
+                </section>
+
+                {/* formulario direita */}
+                <section>
+
+                </section>
+            </form>
         </main>
     )
 }
