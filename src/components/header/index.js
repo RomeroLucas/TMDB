e6cbdf70fefe5dde1d6cc065250eb1e0e48a28f6
Lucas Romero <lucas.romerovivas@gmail.com>
2021-07-10 11:31:44 -0300

    counter do carrinho

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,13 +9,18 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import './style.css'
 
 export default function Header() {
+    const cart = useSelector(state => state.cart)
     const controllers = useSelector(state => state.controllers.aside)
     const dispatch = useDispatch()
     const history = useHistory()
 
+    // controller para abrir e fechar o menu lateral do carrinho
     const handleController = (type) => {
         controllers === false ? dispatch({type: 'OPEN-CART', payload: type}) : dispatch({type: 'CLOSE-CART'}) 
     }
+
+    // contador do carrinho
+    let counter = cart.length > 0 ? <span id='cart-counter'>{cart.length}</span> : null
 
     return (
         <header className='container-header'>
@@ -29,7 +34,7 @@ export default function Header() {
 
             <div>
                 <span onClick={() => handleController('Meus Favoritos')} ><FavoriteIcon /></span>
-                <span onClick={() => handleController('Meu Carrinho')} ><ShoppingCartIcon /></span>
+                <span onClick={() => handleController('Meu Carrinho')} ><ShoppingCartIcon />{counter}</span>
             </div>
         </header>
     )
